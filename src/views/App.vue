b615b9a0ef9a046fb85453705f274f8dc4eb1839
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-02-13 21:42:28 +0100

    JS/Vue: promote initial search string from http request via propertries.

--- src/views/App.vue
+++ src/views/App.vue
@@ -107,6 +107,7 @@
 					:active-layer-id-prop="activeLayerId"
 					:map-bounds-prop="mapBounds"
 					:search-data="searchData"
+					:initial-query="initialQuery"
 					:routing-search-data="routingSearchData"
 					:favorites="displayedFavorites"
 					:favorite-categories="favoriteCategories"
@@ -234,6 +235,7 @@ import { geoToLatLng, getFormattedADR } from '../utils/mapUtils'
 import * as network from '../network'
 import { all as axiosAll, spread as axiosSpread } from 'axios'
 import { generateUrl } from '@nextcloud/router'
+import { loadState } from '@nextcloud/initial-state'
 
 export default {
 	name: 'App',
@@ -256,6 +258,7 @@ export default {
 
 	data() {
 		return {
+		    initialQuery: '',
 		    // Map Options
 		    activeLayerId: optionsController.tileLayer,
 			mapBounds: optionsController.bounds,
@@ -639,6 +642,11 @@ export default {
 		// subscribe('nextcloud:unified-search.search', this.filter)
 		// subscribe('nextcloud:unified-search.reset', this.cleanSearch)
 		setTimeout(() => { emit('files:sidebar:closed') }, 1000)
+		try {
+			this.initialQuery = loadState('maps', 'search')
+		} catch (err) {
+			console.error('Unable to load initial query', err)
+		}
 	},
 	beforeDestroy() {
 		// unsubscribe('nextcloud:unified-search.search', this.filter)
