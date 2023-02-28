5e5eefe03c11eb4cc86bd6515bfcda55bc728779
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-02-28 16:29:36 +0100

    Silence an "initial state not found" error.

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -50,7 +50,7 @@ class PageController extends Controller {
 
 		$params = ['user' => $this->userId];
 		$this->initialStateService->provideInitialState($this->appName, 'photos', $this->config->getAppValue('photos', 'enabled', 'no') === 'yes');
-		$this->initialStateService->provideInitialState($this->appName, 'search', $this->request->getParam('search'));
+		$this->initialStateService->provideInitialState($this->appName, 'search', $this->request->getParam('search') ?? '');
 		$response = new TemplateResponse('maps', 'main', $params);
 
 		$this->addCsp($response);
@@ -68,7 +68,7 @@ class PageController extends Controller {
 
 		$params = ['user' => $this->userId];
 		$this->initialStateService->provideInitialState($this->appName, 'photos', $this->config->getAppValue('photos', 'enabled', 'no') === 'yes');
-		$this->initialStateService->provideInitialState($this->appName, 'search', $this->request->getParam('search'));
+		$this->initialStateService->provideInitialState($this->appName, 'search', $this->request->getParam('search') ?? '');
 		$response = new TemplateResponse('maps', 'main', $params);
 
 		$this->addCsp($response);
